40b902df3b0b61b5b4ed5603c628e762db84d3d3
Abednego Tenge <abednegotenge180@gmail.com>
2025-12-13 19:26:08 +0000

    creating orders and product mutation

--- crm/models.py
+++ crm/models.py
@@ -13,7 +13,7 @@ class Customer(models.Model):
 class Product(models.Model):
     name = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    stock = models.IntegerField(required=False)
+    stock = models.IntegerField()
 
     def __str__(self):
         return self.name
